b12f6bd61f007170536d5e2828280a208b7af632
Christian-bourquin <christian.bourquin23@d155.org>
2023-04-06 08:03:11 -0500

    Initial Commit

---
+++ swiftUiLoginScreen/swiftUiLoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiLoginScreenApp.swift
+//  swiftUiLoginScreen
+//
+//  Created by CHRISTIAN BOURQUIN on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiLoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
